d681f82c69fd33d5b07fce5676e9a3553d7ef397
Student <Student@PC-75>
2018-11-27 09:33:27 +0800

    Travis version "[#162162504]"

--- src/Practical.java
+++ src/Practical.java
@@ -12,5 +12,6 @@ public class Practical {
     public static void main(String[] args){
         System.out.println("Travis");
         System.out.println("Hello");
+        System.out.print("Hello");
     }
 }
